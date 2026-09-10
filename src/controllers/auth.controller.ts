40926d6110ecf2519b0492b8fada9640b21178bd
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-09-10 17:59:18 +0600

    update authcontroller.ts

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -7,9 +7,9 @@ import { ApiError } from '../utils/ApiError.js';
 import { catchAsync } from '../utils/catchAsync.js';
 import { prisma } from '../config/db.js';
 
-
 export const register = catchAsync(async (req: Request, res: Response) => {
-  const { name, email, password } = req.body;
+  // 1.req.boby with destructuring
+  const { name, email, password, role, departmentId } = req.body;
 
   const existingUser = await prisma.user.findUnique({ where: { email } });
   if (existingUser) {
@@ -18,17 +18,21 @@ export const register = catchAsync(async (req: Request, res: Response) => {
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
+  // 2. Create the user in the database
   const user = await prisma.user.create({
     data: {
       name,
       email,
       password: hashedPassword,
+      role: role || 'CITIZEN', // if there is no role
+      departmentId: departmentId || null, // department if null
     },
     select: {
       id: true,
       name: true,
       email: true,
       role: true,
+      departmentId: true, // for resposnse
       createdAt: true,
     },
   });
@@ -49,8 +53,14 @@ export const login = catchAsync(async (req: Request, res: Response) => {
     throw new ApiError(401, 'Invalid email or password');
   }
 
+  // 3.access token for 
   const accessToken = jwt.sign(
-    { id: user.id, email: user.email, role: user.role },
+    { 
+      id: user.id, 
+      email: user.email, 
+      role: user.role, 
+      departmentId: user.departmentId 
+    },
     process.env.JWT_ACCESS_SECRET!,
     { expiresIn: (process.env.JWT_ACCESS_EXPIRES_IN || '1d') as any }
   );
@@ -60,12 +70,14 @@ export const login = catchAsync(async (req: Request, res: Response) => {
     process.env.JWT_REFRESH_SECRET!,
     { expiresIn: (process.env.JWT_REFRESH_EXPIRES_IN || '7d') as any }
   );
+
   return sendSuccess(res, 200, 'Login successful', {
     user: {
       id: user.id,
       name: user.name,
       email: user.email,
       role: user.role,
+      departmentId: user.departmentId, // login response
     },
     accessToken,
     refreshToken,
